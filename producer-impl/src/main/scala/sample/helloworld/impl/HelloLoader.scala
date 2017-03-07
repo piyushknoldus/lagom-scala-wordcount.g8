3a5aa0168ecbadf88650cda91dc68201170c3e77
piyushknoldus <piyush@knoldus.com>
2017-03-07 12:12:56 +0530

    added condcut setting to deploy service

--- producer-impl/src/main/scala/sample/helloworld/impl/HelloLoader.scala
+++ producer-impl/src/main/scala/sample/helloworld/impl/HelloLoader.scala
@@ -3,30 +3,33 @@ package sample.helloworld.impl
 /**
   * Created by knoldus on 16/2/17.
   */
+import com.lightbend.lagom.scaladsl.api.Descriptor
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
-import com.lightbend.lagom.scaladsl.client.ConfigurationServiceLocatorComponents
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraPersistenceComponents
 import com.lightbend.lagom.scaladsl.server._
 import com.softwaremill.macwire._
+import com.typesafe.conductr.bundlelib.lagom.scaladsl.ConductRApplicationComponents
 import play.api.libs.ws.ahc.AhcWSComponents
 import sample.helloworld.api.HelloService
+
+import scala.collection.immutable
 
 class HelloLoader extends LagomApplicationLoader {
 
   override def load(context: LagomApplicationContext): LagomApplication =
-    new HelloApplication(context) with ConfigurationServiceLocatorComponents{
-    }
+    new HelloApplication(context) with ConductRApplicationComponents
 
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
     new HelloApplication(context) with LagomDevModeComponents
+
+  override def describeServices: immutable.Seq[Descriptor] = List(
+    readDescriptor[HelloService]
+  )
 }
 
 abstract class HelloApplication(context: LagomApplicationContext) extends HelloComponents(context)
     with LagomKafkaComponents
-    {
-
-}
 
 abstract class HelloComponents(context: LagomApplicationContext) extends LagomApplication(context)
   with CassandraPersistenceComponents
